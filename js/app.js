d3b7caa455e39a80b8f7b8cd4b09e97ba1d29aa9
jbyoung12 <jbyoung12@gmail.com>
2016-11-19 18:25:07 -0500

    Commit 2

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
+      req.query['hub.verify_token'] === <CORRECT_PAGE>) {
     console.log("Validating webhook");
     res.status(200).send(req.query['hub.challenge']);
   } else {
